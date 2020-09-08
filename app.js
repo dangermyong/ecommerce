c59073caa53f9f29cfdb88514247325a8f31e5ee
dangermyong <66460964+dangermyong@users.noreply.github.com>
2020-09-08 12:13:06 +0900

    user added

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
 require('dotenv').config()
 
 // import routes
@@ -11,7 +15,12 @@ const app = express()
 // db
 mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
   .then(() => { console.log('DB Connected') })
+// middlewares
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser())
 
+  //routes middleware
 app.use('/api', userRoutes)
 
 const port = process.env.PORT || 8000
