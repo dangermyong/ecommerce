6488dffad6c13c2d62a48a36ff4284017537f6e2
dangermyong <66460964+dangermyong@users.noreply.github.com>
2020-09-14 00:11:34 +0900

    before product

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
-const { signup, signin } = require('../controllers/user')
-const { userSignupValidator} = require('../validator/index')
+const { requireSignin, isAuth, isAdmin } = require('../controllers/auth')
 
-router.post('/signup', userSignupValidator, signup)
-router.post('/signin', signin)
+const { userById } = require('../controllers/user')
+
+router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
+  res.json({
+    user: req.profile
+  })
+})
+
+router.param('userId', userById)
 
 module.exports = router
